74b4c0c727c26034c62b542f889d82ddc46e3035
Kevin Melgarejo <91477550+kevan28MY@users.noreply.github.com>
2023-09-19 23:58:15 -0500

    Add files via upload

---
+++ laboratorio 2/Problema 2/array2.php
@@ -0,0 +1,21 @@
+<?php
+$imparess=[1,3,5,7,9,11,13,15,17,19];
+
+?>
+
+
+<html>
+    <h3>El numero impar en la posicion 1 es: <?php echo $impares[0]?></h3>
+    <h3>El numero impar en la posicion 2 es: <?php echo $impares[1]?></h3>
+    <h3>El numero impar en la posicion 3 es: <?php echo $impares[2]?></h3>
+    <h3>El numero impar en la posicion 4 es: <?php echo $impares[3]?></h3>
+    <h3>El numero impar en la posicion 5 es: <?php echo $impares[4]?></h3>
+    <h3>El numero impar en la posicion 6 es: <?php echo $impares[5]?></h3>
+    <h3>El numero impar en la posicion 7 es: <?php echo $impares[6]?></h3>
+    <h3>El numero impar en la posicion 8 es: <?php echo $impares[7]?></h3>
+    <h3>El numero impar en la posicion 9 es: <?php echo $impares[8]?></h3>
+    <h3>El numero impar en la posicion 10 es: <?php echo $impares[9]?></h3>
+
+
+    <h3>Los primeros 10 numero impares son: <?php echo $impares[1]?> </h3>
+</html>
---
+++ laboratorio 2/S2 Ejercicio 1/ejercicio2.php
@@ -0,0 +1,11 @@
+<?php
+$frutas=['Manzana','Uva','Fresa'];
+
+?>
+
+
+<html>
+    <h3>La fruta 1 es: <?php echo $frutas[0]?></h3>
+    <h3>La fruta 2 es: <?php echo $frutas[1]?></h3>
+    <h3>La fruta 3 es: <?php echo $frutas[2]?></h3>
+</html>
